e47ae7008b24fcb51cdb958cbdf6cca3d5550dcf
Anton Shelanov <shelanov.anton@yandex.ru>
2022-11-11 00:45:43 +0300

    adaptiv main page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,22 +6,23 @@ import logo from '../../images/header_logo.svg'
 function Header(props) {
     return (
       <header className={`Header${props.isAbout ? ' Header_backgroud_blue' : ''}`}>
-        <img className="Header__logo" src={logo} alt="logo"></img>
         {props.isAbout 
           ? 
-            <>
+            <div className="Header__about">
+              <img className="Header__logo" src={logo} alt="logo"></img>
               <Link className="Header__regLink" to="/signup">Регистрация</Link>
               <Link className="Header__logLink" to="/signin">Войти </Link>
-            </>
+            </div>
           :
             <>
+              <Link to="/"><img className="Header__logo" src={logo} alt="logo"></img></Link>
               <nav className="Header__menu">
                 <NavLink to="/movies" activeClassName="Header__menuLink_active" className="Header__menuLink">Фильмы</NavLink>
                 <NavLink to="/saved-movies" activeClassName="Header__menuLink_active" className="Header__menuLink">Сохраненные фильмы</NavLink>
               </nav>
               <div className="Header__profileGroup">
                 <p className="Header__profileText">Аккаунт</p>
-                <button type="button" className="Header__profileLogo" aria-label="Profile"></button>
+                <Link className="Header__profileLogo" to="/profile"></Link>
               </div>
             </>}
       </header>
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -5,11 +5,11 @@ function Promo() {
     return (
         <section className="Promo">
             <div className="Promo__info">
+                <img className="Promo__img" src={img} alt="logo"></img>
                 <div className="Promo__text">
                     <h1 className="Promo__title">Учебный проект студента факультета Веб&#8288;-&#8288;разработки.</h1>
                     <p className="Promo__notice">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
                 </div>
-                <img className="Promo__img" src={img} alt="logo"></img>
             </div>
             <button type="button" className="Promo__btn">Узнать больше</button>
         </section>
