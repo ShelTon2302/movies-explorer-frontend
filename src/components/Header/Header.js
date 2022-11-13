1a2ca3dd167538aff54e20eb9abb45a439832b48
Anton Shelanov <shelanov.anton@yandex.ru>
2022-11-13 14:05:51 +0300

    adaptiv

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
-import logo from '../../images/header_logo.svg'
+import logo from '../../images/header_logo.svg';
+import Navigation from '../Navigation/Navigation';
 
 function Header(props) {
-    return (
-      <header className={`Header${props.isAbout ? ' Header_backgroud_blue' : ''}`}>
-        {props.isAbout 
-          ? 
-            <div className="Header__content">
-              <img className="Header__logo" src={logo} alt="logo"></img>
-              <Link className="Header__regLink" to="/signup">Регистрация</Link>
-              <Link className="Header__logLink" to="/signin">Войти </Link>
-            </div>
-          :
-            <div className="Header__content">
-              <Link to="/"><img className="Header__logo" src={logo} alt="logo"></img></Link>
-              <nav className="Header__menu">
-                <NavLink to="/movies" activeClassName="Header__menuLink_active" className="Header__menuLink">Фильмы</NavLink>
-                <NavLink to="/saved-movies" activeClassName="Header__menuLink_active" className="Header__menuLink">Сохраненные фильмы</NavLink>
-              </nav>
-              <div className="Header__profileGroup">
-                <p className="Header__profileText">Аккаунт</p>
-                <Link className="Header__profileLogo" to="/profile"></Link>
-              </div>
-            </div>}
-      </header>
-    );
+  const [enableMenu, setEnableMenu] = React.useState(false);
+
+  function openMenu() {
+    setEnableMenu(true);
+  }
+
+  function closeMenu() {
+    setEnableMenu(false);
+  }
+
+  return (
+    <header className={`Header${props.isAbout ? ' Header_backgroud_blue' : ''}`}>
+      <Link className="Header__link" to="/"><img className="Header__logo" src={logo} alt="logo"></img></Link>
+      {props.isAbout 
+        ? 
+          <div className="Header__aboutContent">
+            {/*<img className="Header__logo" src={logo} alt="logo"></img>*/}
+            <Link className="Header__regLink" to="/signup">Регистрация</Link>
+            <Link className="Header__logLink" to="/signin">Войти </Link>
+          </div>
+        :
+          <div className={`Header__content${enableMenu ? ' Header__content_visibility_enable' : ''}`}>
+            <button type='button' className="Header__closeButton" onClick={closeMenu} hidden={!enableMenu}></button>
+            <button type='button' className="Header__button" onClick={openMenu} hidden={enableMenu}></button>
+
+            <Navigation />
+
+            {/*<Link to="/"><img className="Header__logo" src={logo} alt="logo"></img></Link>
+            <nav className="Header__menu">
+              <NavLink to="/movies" activeClassName="Header__menuLink_active" className="Header__menuLink">Фильмы</NavLink>
+              <NavLink to="/saved-movies" activeClassName="Header__menuLink_active" className="Header__menuLink">Сохраненные фильмы</NavLink>
+            </nav>
+            <div className="Header__profileGroup">
+              <p className="Header__profileText">Аккаунт</p>
+              <Link className="Header__profileLogo" to="/profile"></Link>
+            </div>*/}
+          </div>}
+
+    </header>
+  );
   }
   
   export default Header;
