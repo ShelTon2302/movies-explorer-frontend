9419354d5e629a68803cd1670d97ddf1185d1001
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-11-16 21:24:54 +0300

    review again

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -5,7 +5,7 @@ function MoviesCard(props) {
 
     return (
         <div className="MoviesCard">
-            <div className="MoviesCard__info">
+            {/*<div className="MoviesCard__info">
                 <h4 className="MoviesCard__title">{props.nameRU}</h4>
                 <p className="MoviesCard__duration">{props.duration}</p>
                 {props.isSaved
@@ -15,8 +15,28 @@ function MoviesCard(props) {
                     <button type="button" className={cardLikeButtonClassName} aria-label="Нравится"></button>
                 }
             </div>
-            <img className="MoviesCard__thumbnail" src={props.thumbnail} alt="thumbnail "></img>
+            <img className="MoviesCard__thumbnail" src={props.thumbnail} alt="thumbnail "></img>*/}
+            <ul className="MoviesCard__item">
+                <li className="MoviesCard__element">
+                    <h4 className="MoviesCard__title">{props.nameRU}</h4>
+                </li>
+                <li className="MoviesCard__element">
+                    <p className="MoviesCard__duration">{props.duration}</p>
+                </li>
+                <li className="MoviesCard__element">
+                    {props.isSaved
+                    ?
+                        <button type="button" className="MovieCard__delete" aria-label="Удалить"></button>
+                    :
+                        <button type="button" className={cardLikeButtonClassName} aria-label="Нравится"></button>
+                    }
+                </li>
+                <li className="MoviesCard__element">
+                    <img className="MoviesCard__thumbnail" src={props.thumbnail} alt="thumbnail "></img>
+                </li>
+            </ul>
         </div>
+
     )
 };
 
