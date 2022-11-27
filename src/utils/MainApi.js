a1147d3ca80885b79244ff645f671feeac9b255e
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-11-27 22:58:16 +0300

    before review

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -122,7 +122,7 @@ class Api {
 }
 
 const api = new Api ({
-    baseUrl: 'http://localhost:3001', /*'https://api.project-mesto.nomoredomains.icu',*/
+    baseUrl: 'https://api.project-mesto.nomoredomains.icu',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
